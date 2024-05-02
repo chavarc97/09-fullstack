a60abf714c0b00432862c260f72a88a97b18d936
Salvador Rodriguez <chava.crb@gmail.com>
2024-05-01 18:17:15 -0600

    action-runners test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 function App() {
   return (
     <>
-
+   <div className="flex items-center justify-center h-screen">
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        click
+    </button>
+</div>
     </>
   )
 }
